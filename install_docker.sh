1904e3c5951f92bfcdd19c90d6bd435548e1d330
fqaiser94 <fqaiser94@gmail.com>
2018-12-25 15:41:53 -0500

    install docker

---
+++ install_docker.sh
@@ -0,0 +1,5 @@
+https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+https://docs.docker.com/v17.09/engine/installation/linux/linux-postinstall/#manage-docker-as-a-non-root-user
+
+sudo chmod 666 /var/run/docker.sock
