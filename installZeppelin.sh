9fe74f99216758d23b818e03237c10397136f568
fqaiser94 <fqaiser94@gmail.com>
2019-04-24 19:17:29 -0400

    fixed bug, zeppelin will now respect installed spark version

--- installZeppelin.sh
+++ installZeppelin.sh
@@ -1,6 +1,8 @@
 wget http://www-us.apache.org/dist/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz
 tar xf zeppelin-*-bin-all.tgz -C /opt
 mv /opt/zeppelin-*-bin-all /opt/zeppelin
+mv /opt/zeppelin/conf/zeppelin-env.sh.template /opt/zeppelin/conf/zeppelin-env.sh
+echo 'export SPARK_HOME=/usr/lib/spark' >> ~/opt/zeppelin/conf/zeppelin-env.sh
 rm zeppelin-0.8.1-bin-all.tgz
 
 useradd -d /opt/zeppelin -s /bin/false zeppelin
