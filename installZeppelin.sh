84242f96ed4d061fff97484b1ace942b13218906
fqaiser94 <fqaiser94@gmail.com>
2019-04-24 16:20:20 -0400

    added systemd template for zeppelin

--- installZeppelin.sh
+++ installZeppelin.sh
@@ -2,3 +2,8 @@ wget http://www-us.apache.org/dist/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-al
 tar xf zeppelin-*-bin-all.tgz -C /opt
 mv /opt/zeppelin-*-bin-all /opt/zeppelin
 rm zeppelin-0.8.1-bin-all.tgz
+
+useradd -d /opt/zeppelin -s /bin/false zeppelin
+chown -R zeppelin:zeppelin /opt/zeppelin
+cp zeppelin.service.template /etc/systemd/system/zeppelin.service
+systemctl start zeppelin
